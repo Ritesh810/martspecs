9e4bf2e8bd4f036aa3ce3afc458098b4854ca2ff
Ritesh Narola <riteshnarola810@gmail.com>
2025-10-14 11:48:25 +0530

    fixed packages errors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-// Export all your components
+// Export all your components for use as a library
 export { default as CallToAction } from './atomic/organism/call-to-action-new';
 export { default as ButtonApple } from './atomic/atom/button-apple';
 export { default as ImageI18N } from './atomic/atom/img-i18n';
-
-// Export types
-export type { CallToActionProps } from './atomic/organism/call-to-action-new';
 
 // Export i18n
 export { default as _ } from './i18n/locale';
