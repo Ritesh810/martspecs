f868b3899f9bd4bf8ad72dd78153469da9c70bf3
Ritesh Narola <riteshnarola810@gmail.com>
2025-10-14 11:31:49 +0530

    next js file removed and yarn reinstall

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Export all your components
+export { default as CallToAction } from './atomic/organism/call-to-action-new';
+export { default as ButtonApple } from './atomic/atom/button-apple';
+export { default as ImageI18N } from './atomic/atom/img-i18n';
+
+// Export types
+export type { CallToActionProps } from './atomic/organism/call-to-action-new';
+
+// Export i18n
+export { default as _ } from './i18n/locale';
