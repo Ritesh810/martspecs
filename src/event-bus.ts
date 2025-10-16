e3c598d507f5921c542d190d5eb3f88c54655372
Ritesh Narola <riteshnarola810@gmail.com>
2025-10-16 12:32:43 +0530

    event bus file function changed

--- src/event-bus.ts
+++ src/event-bus.ts
@@ -1,41 +1,39 @@
+type EventCallback = (...args: any[]) => void;
+
 class EventBus {
-    private callbacksByEvents = new Map<string, { (payload: any): void }[]>();
+  private events: Map<string, EventCallback[]> = new Map();
 
-    send(eventName: string, payload: any = {}, onDone: (res: any) => void = (res) => {}) {
-        if (this.callbacksByEvents.has(eventName)) {
-            const callbacks = this.getCallbacks(eventName);
-
-            for (let event of callbacks) onDone(event(payload));
-        }
+  addEvent(eventName: string, callback: EventCallback): void {
+    if (!this.events.has(eventName)) {
+      this.events.set(eventName, []);
     }
+    this.events.get(eventName)!.push(callback);
+  }
 
-    addEvent(eventName: string, callback: (payload: any) => void) {
-        if (this.callbacksByEvents.has(eventName)) {
-            const callbacks = this.getCallbacks(eventName);
-
-            callbacks.push(callback);
-        } else {
-            this.callbacksByEvents.set(eventName, [callback]);
-        }
+  removeEvent(eventName: string, callback: EventCallback): void {
+    const callbacks = this.events.get(eventName);
+    if (callbacks) {
+      const index = callbacks.indexOf(callback);
+      if (index > -1) {
+        callbacks.splice(index, 1);
+      }
     }
+  }
 
-    removeEvent(eventName: string) {
-        this.callbacksByEvents.delete(eventName);
+  emit(eventName: string, ...args: any[]): void {
+    const callbacks = this.events.get(eventName);
+    if (callbacks) {
+      callbacks.forEach(callback => callback(...args));
     }
+  }
 
-    removeCallback(callback: (payload: any) => void) {
-        for (let events of this.callbacksByEvents.values()) {
-            let idx = events.findIndex((event) => event == callback);
-            if (idx != -1) events.splice(idx, 1);
-        }
+  removeAllEvents(eventName?: string): void {
+    if (eventName) {
+      this.events.delete(eventName);
+    } else {
+      this.events.clear();
     }
-
-    getCallbacks(eventName: string): ((payload: any) => void)[] {
-        const callbacks = this.callbacksByEvents.get(eventName);
-        if (!callbacks) throw new Error(`callbacks triggered by event:"${eventName}" were not found`);
-
-        return callbacks;
-    }
+  }
 }
 
 export default new EventBus();
